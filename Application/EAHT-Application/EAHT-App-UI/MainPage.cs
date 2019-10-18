e95f73c4fcf11c404ca1a5540c2144a52e8d056d
Joao Marques <joao.goncalves-marques@student.anglia.ac.uk>
2019-10-18 18:28:56 +0100

    Add files via upload

---
+++ Application/EAHT-Application/EAHT-App-UI/MainPage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EAHT_App_UI
+{
+    public partial class MainPage : Form
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void MainPage_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+            Console.WriteLine("Insert your credentials");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //By clicking on this button, moves to another form, in this case to Managemnet report
+            Management_Report_Interface management_Report_Interface = new Management_Report_Interface();
+            management_Report_Interface.ShowDialog();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //It goes to Login page
+            login_page login_Page = new login_page();
+            login_Page.ShowDialog();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
