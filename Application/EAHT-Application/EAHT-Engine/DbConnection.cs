94b7caec2a40febd5a9bb1ad724b22cdb88e1c51
aliasmaus <louise.amas@student.anglia.ac.uk>
2019-10-19 15:39:19 +0100

    DbConnection class created + some tidying up

---
+++ Application/EAHT-Application/EAHT-Engine/DbConnection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EAHT_Engine
+{
+    /// <summary>
+    /// Class to handle connection and data retrieval methods for the application
+    /// Guide can be found in week 5 lecture slides
+    /// Much of the code below has been copied and modified from the slides and the originals are included for reference
+    /// </summary>
+    class DbConnection
+    {
+        /// <summary>
+        /// Below attributes are taken from week 5 lecture slides; Original code:
+        /// <code>
+        /// privatestringconnectionStr;
+        /// //the SqlConnection object used to store the connection to the database 
+        /// System.Data.SqlClient.SqlConnection connectionToDB;
+        /// // the DataAdapter object used to open a table of the database
+        /// private System.Data.SqlClient.SqlDataAdapter dataAdapter;
+        /// </code>
+        /// </summary>
+        private string connectionString;
+        private System.Data.SqlClient.SqlConnection dbConnection;
+        private System.Data.SqlClient.SqlDataAdapter dataAdapter;
+    }
+}
