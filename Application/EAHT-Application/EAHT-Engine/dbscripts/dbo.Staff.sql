000c4b80cf39df70c2bdd383d66ed89b037558cb
aliasmaus <louise.amas@student.anglia.ac.uk>
2019-11-05 10:02:38 +0000

    More tables in database
    -Staff
    -Staff_Logins

---
+++ Application/EAHT-Application/EAHT-Engine/dbscripts/dbo.Staff.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Staff]
+(
+	[NHS_ID] CHAR(10) NOT NULL PRIMARY KEY, 
+    [First_Name] VARCHAR(50) NULL, 
+    [Last_Name] VARCHAR(50) NULL
+)
---
+++ Application/EAHT-Application/EAHT-Engine/dbscripts/dbo.Staff_Logins.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Staff_Logins]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Staff_Member_ID] CHAR(10) NOT NULL, 
+    [Log_In_Time] DATETIME NOT NULL, 
+    [Log_Out_Time] DATETIME NULL, 
+    CONSTRAINT [FK_Staff_Logins_ToStaff] FOREIGN KEY ([Staff_Member_ID]) REFERENCES [Staff]([NHS_ID])
+)
