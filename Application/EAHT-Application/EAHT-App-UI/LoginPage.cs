447611af27660cd76c5e7e2e9ee7bff7a2007832
Joao <joao.goncalves-marques@student.anglia.ac.uk>
2019-11-17 20:09:59 +0000

    Comments Added
    Comments added on the code

--- Application/EAHT-Application/EAHT-App-UI/LoginPage.cs
+++ Application/EAHT-Application/EAHT-App-UI/LoginPage.cs
@@ -25,11 +25,17 @@ namespace EAHT_App_UI
 
         private void Login_Click(object sender, EventArgs e)
         {
+                    
+       
+            // It allows comunication between the Database's source and the application
             System.Data.SqlClient.SqlConnection dbConnection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\EAHT - Monitor - App\Application\EAHT - Application\EAHT - Engine\EAHT - Database.mdf; Integrated Security = True; Connect Timeout = 30");
+
+            //SQL query. 
             SqlDataAdapter dataAdapter= new SqlDataAdapter("SELECT * FROM Login WHERE username = ' " + textBox1.Text + " 'AND password = ' " + textBox2.Text + "'", dbConnection);
             DataTable data = new DataTable();
             dataAdapter.Fill(data);
 
+            //If the all rows of the Login's table are matching, the login is sucessful and directs to main menu
             if (data.Rows.Count == 1)
             {
                 MainPage mainPage = new MainPage();
@@ -38,9 +44,10 @@ namespace EAHT_App_UI
             }
             else
             {
+                //Error message
                 MessageBox.Show("Invalid Username or Password. Please insert your details again");
             }
-            
+            //It needs to be tested!!! It was working yesterday, before the merges.
         }
     }
 }
