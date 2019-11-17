e2a2cb8e60662cc6e3fdabae836c59639a49d2a9
Lawrence <lawrence.ball@hotmail.co.uk>
2019-11-17 14:37:03 +0000

    Login code/ unfinished/ very broken...still

--- Application/EAHT-Application/EAHT-App-UI/Login.Designer.cs
+++ Application/EAHT-Application/EAHT-App-UI/Login.Designer.cs
@@ -40,10 +40,9 @@
             // 
             this.Username.AutoSize = true;
             this.Username.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Username.Location = new System.Drawing.Point(173, 123);
-            this.Username.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Username.Location = new System.Drawing.Point(130, 100);
             this.Username.Name = "Username";
-            this.Username.Size = new System.Drawing.Size(127, 33);
+            this.Username.Size = new System.Drawing.Size(98, 26);
             this.Username.TabIndex = 0;
             this.Username.Text = "Username";
             // 
@@ -51,20 +50,18 @@
             // 
             this.Password.AutoSize = true;
             this.Password.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Password.Location = new System.Drawing.Point(173, 231);
-            this.Password.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Password.Location = new System.Drawing.Point(130, 188);
             this.Password.Name = "Password";
-            this.Password.Size = new System.Drawing.Size(119, 33);
+            this.Password.Size = new System.Drawing.Size(93, 26);
             this.Password.TabIndex = 1;
             this.Password.Text = "Password";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(653, 380);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(490, 309);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(171, 68);
+            this.button1.Size = new System.Drawing.Size(128, 55);
             this.button1.TabIndex = 2;
             this.button1.Text = "Exit";
             this.button1.UseVisualStyleBackColor = true;
@@ -73,47 +70,45 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Calibri", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(249, 380);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Location = new System.Drawing.Point(187, 309);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(171, 68);
+            this.button2.Size = new System.Drawing.Size(128, 55);
             this.button2.TabIndex = 3;
             this.button2.Text = "Login";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(396, 123);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(297, 100);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(464, 22);
+            this.textBox1.Size = new System.Drawing.Size(349, 20);
             this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(396, 231);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(297, 188);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(464, 22);
+            this.textBox2.Size = new System.Drawing.Size(349, 20);
             this.textBox2.TabIndex = 5;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.BurlyWood;
-            this.ClientSize = new System.Drawing.Size(1123, 626);
+            this.ClientSize = new System.Drawing.Size(842, 509);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.Password);
             this.Controls.Add(this.Username);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Login";
             this.RightToLeftLayout = true;
             this.Text = "Login";
             this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Login_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
