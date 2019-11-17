52524c563bcd0b574387d18c93ff8b7f48bf5928
Joao <joao.goncalves-marques@student.anglia.ac.uk>
2019-11-17 20:01:40 +0000

    Code previously written, following the User Stories
    After merging a few files, the code previously written was lost.  Login page using  the Database tables with SQL query. It needs to be tested,but it hasn't been due to a few errors on another "login page".

--- Application/EAHT-Application/EAHT-App-UI/LoginPage.Designer.cs
+++ Application/EAHT-Application/EAHT-App-UI/LoginPage.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace EAHT_App_UI
 {
-    partial class login_page
+    partial class LoginPage
     {
         /// <summary>
         /// Required designer variable.
@@ -64,16 +64,16 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(294, 106);
+            this.textBox1.Location = new System.Drawing.Point(294, 109);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(297, 32);
+            this.textBox1.Size = new System.Drawing.Size(297, 38);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(294, 200);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(297, 32);
+            this.textBox2.Size = new System.Drawing.Size(297, 38);
             this.textBox2.TabIndex = 3;
             // 
             // Username
@@ -84,7 +84,7 @@
             this.Username.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.Username.Location = new System.Drawing.Point(153, 109);
             this.Username.Name = "Username";
-            this.Username.Size = new System.Drawing.Size(95, 24);
+            this.Username.Size = new System.Drawing.Size(118, 31);
             this.Username.TabIndex = 4;
             this.Username.Tag = "Username";
             this.Username.Text = "Username";
@@ -97,18 +97,18 @@
             this.Password.BackColor = System.Drawing.SystemColors.Control;
             this.Password.Location = new System.Drawing.Point(153, 200);
             this.Password.Name = "Password";
-            this.Password.Size = new System.Drawing.Size(89, 24);
+            this.Password.Size = new System.Drawing.Size(111, 31);
             this.Password.TabIndex = 5;
             this.Password.Tag = "Password";
             this.Password.Text = "Password";
             // 
-            // login_page
+            // LoginPage
             // 
             this.AccessibleDescription = "Login Page";
             this.AccessibleName = "Login Page";
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackColor = System.Drawing.Color.BurlyWood;
             this.ClientSize = new System.Drawing.Size(703, 441);
             this.Controls.Add(this.Password);
             this.Controls.Add(this.Username);
@@ -118,7 +118,7 @@
             this.Controls.Add(this.Exit);
             this.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
-            this.Name = "login_page";
+            this.Name = "LoginPage";
             this.Tag = "Login Page";
             this.Text = "Login Page";
             this.ResumeLayout(false);
--- Application/EAHT-Application/EAHT-App-UI/LoginPage.cs
+++ Application/EAHT-Application/EAHT-App-UI/LoginPage.cs
@@ -7,12 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace EAHT_App_UI
 {
-    public partial class login_page : Form
+    public partial class LoginPage : Form
     {
-        public login_page()
+        public LoginPage()
         {
             InitializeComponent();
         }
@@ -24,7 +25,22 @@ namespace EAHT_App_UI
 
         private void Login_Click(object sender, EventArgs e)
         {
-            //goto whatever page is next
+            System.Data.SqlClient.SqlConnection dbConnection = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = C:\EAHT - Monitor - App\Application\EAHT - Application\EAHT - Engine\EAHT - Database.mdf; Integrated Security = True; Connect Timeout = 30");
+            SqlDataAdapter dataAdapter= new SqlDataAdapter("SELECT * FROM Login WHERE username = ' " + textBox1.Text + " 'AND password = ' " + textBox2.Text + "'", dbConnection);
+            DataTable data = new DataTable();
+            dataAdapter.Fill(data);
+
+            if (data.Rows.Count == 1)
+            {
+                MainPage mainPage = new MainPage();
+                this.Hide();
+                mainPage.Show();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Username or Password. Please insert your details again");
+            }
+            
         }
     }
 }
