5d5e46d489ed521e260a778cd48ab249e77be35d
Joao <joao.goncalves-marques@student.anglia.ac.uk>
2019-12-15 17:09:56 +0000

    LoginTest
    Login Test made.

---
+++ Application/EAHT-Application/EAHT-EngineTests/Login.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EAHT_EngineTests
+{
+    [TestClass]
+    public class Login
+    {
+        [TestMethod]
+        public void CheckPassword()
+        {
+            string username = "";
+            string password = "";
+            string userResponse = "";
+            userResponse = username;
+            username = password;
+        }
+    }
+}
